25803c77d4cc7c62eacbf8ef1b0213b67633ecde
Tapia03 <rodrigorosalestapia@gmail.com>
2025-08-30 02:35:05 -0300

    Commit teste para railway

--- src/main/java/meu/primeiro/api/controller/UserController.java
+++ src/main/java/meu/primeiro/api/controller/UserController.java
@@ -1,19 +1,15 @@
 package meu.primeiro.api.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import java.util.ArrayList;
 
 @RestController
+@RequestMapping ("/users")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping
     public List<String> getAllUsers() {
-        List<String> users = new ArrayList<>();
-        users.add("Rodrigo");
-        users.add("Maria");
-        users.add("João");
-        return users;
+        return List.of("Rodrigo", "Maria", "João");
     }
-
 }
