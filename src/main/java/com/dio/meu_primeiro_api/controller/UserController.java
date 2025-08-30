e521f6f955cfaf271fb4540069f476b7db8e3cbc
Tapia03 <rodrigorosalestapia@gmail.com>
2025-08-30 02:20:39 -0300

    Adiciona endpoint /users

--- src/main/java/com/dio/meu_primeiro_api/controller/UserController.java
+++ src/main/java/com/dio/meu_primeiro_api/controller/UserController.java
@@ -1,13 +1,15 @@
-package com.dio.meu_primeiro_api.controller;
+package com.dio.meuprimeiroapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Arrays;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public String getUsers() {
-        return "Hello Users!";
+    public List<String> getUsers() {
+        return Arrays.asList("Rodrigo", "Ana", "Maria");
     }
 }
