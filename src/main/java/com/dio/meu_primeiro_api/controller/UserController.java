c8f7d9f31870dcde033787983a4f6b290ad31b6c
Tapia03 <rodrigorosalestapia@gmail.com>
2025-08-30 02:23:43 -0300

    agora vai

--- src/main/java/com/dio/meu_primeiro_api/controller/UserController.java
+++ src/main/java/com/dio/meu_primeiro_api/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.dio.meuprimeiroapi.controller;
+package com.dio.meu_primeiro_api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
