1595df3a3a49a4ae053c7da226f5c9981461bc3a
Tapia03 <rodrigorosalestapia@gmail.com>
2025-08-30 02:17:26 -0300

    Configura porta do Railway

--- src/main/java/com/dio/meu_primeiro_api/DioMeuPrimeiroApiApplication.java
+++ src/main/java/com/dio/meu_primeiro_api/DioMeuPrimeiroApiApplication.java
@@ -3,9 +3,14 @@ package com.dio.meu_primeiro_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class DioMeuPrimeiroApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DioMeuPrimeiroApiApplication.class, args);
+		SpringApplication app = new SpringApplication(DioMeuPrimeiroApiApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+		app.run(args);
 	}
 }
+
